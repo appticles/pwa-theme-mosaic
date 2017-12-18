2f8393dbd4ec3b2b5493d7f6343a00069d3f4b87
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-18 15:25:07 +0200

    navbar reducer test

--- src/components/NavBar/reducer.js
+++ src/components/NavBar/reducer.js
@@ -1,5 +1,7 @@
 import { MENU_OPEN, MENU_CLOSE } from './action';
 
+export const INITIAL_STATE = false;
+
 export default function (state = false, action) {
   switch (action.type) {
   case MENU_OPEN:
---
+++ src/components/NavBar/reducer.test.js
@@ -0,0 +1,16 @@
+import navBar, { INITIAL_STATE } from './reducer';
+import { openMenu, closeMenu } from './action';
+
+describe('NavBar reducer', () => {
+  it('returns the same state on an unhandled action', () => {
+    expect(navBar(INITIAL_STATE, { type: '_NULL' })).toMatchSnapshot();
+  });
+
+  it('handles OPEN_MENU action', () => {
+    expect(navBar(INITIAL_STATE, openMenu())).toMatchSnapshot();
+  });
+
+  it('handles CLOSE_MENU action', () => {
+    expect(navBar(INITIAL_STATE, closeMenu())).toMatchSnapshot();
+  });
+});
