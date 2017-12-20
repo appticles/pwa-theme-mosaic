4b5f559827b8548a5f59d91fd5669d612c623e6f
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-20 13:42:41 +0200

    refactor posts list component to render even/odd no of posts

--- src/components/PostsList/__test__/index.test.js
+++ src/components/PostsList/__test__/index.test.js
@@ -5,8 +5,8 @@ import { shallowToJson } from 'enzyme-to-json';
 import PostsList from '../index';
 
 describe('Posts list', () => {
-  it('should render correctly', () => {
-    const mockPosts = [
+  it('should render correctly even number of posts', () => {
+    const mockEvenPosts = [
       {
         id: 1,
         title: {
@@ -50,7 +50,35 @@ describe('Posts list', () => {
         },
       },
     ];
-    const output = shallow(<PostsList postsList={mockPosts} />);
+    const output = shallow(<PostsList postsList={mockEvenPosts} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should render correctly odd number of posts', () => {
+    const mockOddPosts = [
+      {
+        id: 1,
+        title: {
+          rendered: 'Mock title',
+        },
+        author: 55,
+        categories: [43, 54, 23],
+        slug: 'mock-slug',
+        content: {
+          rendered: 'Some mock content to be rendered',
+          protected: false,
+        },
+        excerpt: {
+          rendered: 'Some mock exceprt to be rendered',
+          protected: true,
+        },
+        date: 'some mock date',
+        _embedded: {
+          'wp:featuredmedia': 'someMockImageUrl',
+        },
+      },
+    ];
+    const output = shallow(<PostsList postsList={mockOddPosts} />);
     expect(shallowToJson(output)).toMatchSnapshot();
   });
 });
