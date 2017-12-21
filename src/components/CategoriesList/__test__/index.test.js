1c51977a3f50d180d98473a0a3f24a8b5332928a
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-21 11:12:10 +0200

    refactor categories list component test

--- src/components/CategoriesList/__test__/index.test.js
+++ src/components/CategoriesList/__test__/index.test.js
@@ -5,7 +5,7 @@ import { shallowToJson } from 'enzyme-to-json';
 import CategoriesList from '../index';
 
 describe('Categories list', () => {
-  it('should render correctly', () => {
+  it('should render correctly 5 categories', () => {
     const mockCategoriesList = [
       {
         slug: 'mock-slug',
@@ -32,22 +32,91 @@ describe('Categories list', () => {
         id: 5,
         name: 'Some other other mock name',
       },
+    ];
+
+    const output = shallow(<CategoriesList categoriesChunk={mockCategoriesList} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+  it('should render correctly 4 categories', () => {
+    const mockCategoriesList = [
       {
         slug: 'mock-slug',
-        id: 6,
-        name: 'Some other other mock name',
+        id: 1,
+        name: 'Some mock name',
       },
       {
         slug: 'mock-slug',
-        id: 7,
-        name: 'Some other other mock name',
+        id: 2,
+        name: 'Some other mock name',
       },
       {
         slug: 'mock-slug',
-        id: 8,
-        name: 'Some other other mock name',
+        id: 3,
+        name: 'Some name',
+      },
+      {
+        slug: 'mock-slug',
+        id: 4,
+        name: 'Mock name',
       },
     ];
+
+    const output = shallow(<CategoriesList categoriesChunk={mockCategoriesList} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+  it('should render correctly 3 categories', () => {
+    const mockCategoriesList = [
+      {
+        slug: 'mock-slug',
+        id: 1,
+        name: 'Some mock name',
+      },
+      {
+        slug: 'mock-slug',
+        id: 2,
+        name: 'Some other mock name',
+      },
+      {
+        slug: 'mock-slug',
+        id: 3,
+        name: 'Some name',
+      },
+    ];
+
+    const output = shallow(<CategoriesList categoriesChunk={mockCategoriesList} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+  it('should render correctly 2 categories', () => {
+    const mockCategoriesList = [
+      {
+        slug: 'mock-slug',
+        id: 1,
+        name: 'Some mock name',
+      },
+      {
+        slug: 'mock-slug',
+        id: 2,
+        name: 'Some other mock name',
+      },
+    ];
+
+    const output = shallow(<CategoriesList categoriesChunk={mockCategoriesList} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+  it('should render correctly one category', () => {
+    const mockCategoriesList = [
+      {
+        slug: 'mock-slug',
+        id: 1,
+        name: 'Some mock name',
+      },
+    ];
+
+    const output = shallow(<CategoriesList categoriesChunk={mockCategoriesList} />);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+  it('should render correctly no category', () => {
+    const mockCategoriesList = [];
 
     const output = shallow(<CategoriesList categoriesChunk={mockCategoriesList} />);
     expect(shallowToJson(output)).toMatchSnapshot();
