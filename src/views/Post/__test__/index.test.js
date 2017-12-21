9fa2b01f2b56c587f43c2a1466af2fa8105ea345
FlorinaIe <florina.ieremciuc@gmail.com>
2017-12-21 14:21:14 +0200

    test post component

---
+++ src/views/Post/__test__/index.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import mockStore from '../../../configureMockStore';
+import Post from '../index';
+
+describe('Post screen', () => {
+  it('should render correctly that a post does not exist', () => {
+    const store = mockStore({
+      posts: {
+        items: [],
+        isFetching: 1,
+      },
+    });
+    const match = {
+      params: { postId: 'some-post-id' },
+    };
+    expect(renderer.create(<Post store={store} fetchPosts={jest.fn()} match={match} />)).toMatchSnapshot();
+  });
+  it('should render correctly an existing post', () => {
+    const store = mockStore({
+      posts: {
+        items: [
+          {
+            id: 1,
+            title: {
+              rendered: 'Some post title',
+            },
+            author: 54,
+            categories: [1, 4, 2],
+            slug: 'some-post-slug',
+            content: {
+              rendered: 'Some post content to be rendered',
+              protected: true,
+            },
+            excerpt: {
+              rendered: 'Some post excerpt to be rendered',
+              protected: false,
+            },
+            date: 'some mock date',
+          },
+        ],
+        isFetching: 1,
+      },
+    });
+    const match = {
+      params: { postId: 'some-post-id' },
+    };
+    expect(renderer.create(<Post store={store} fetchPosts={jest.fn()} match={match} />)).toMatchSnapshot();
+  });
+});
