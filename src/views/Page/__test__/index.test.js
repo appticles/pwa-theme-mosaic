274d9ddf6f20f280ddf12fabd40f85115c433bba
FlorinaIe <florina.ieremciuc@gmail.com>
2018-01-04 11:59:03 +0200

    test page view

--- src/views/Page/__test__/index.test.js
+++ src/views/Page/__test__/index.test.js
@@ -3,10 +3,25 @@ import renderer from 'react-test-renderer';
 
 import mockStore from '../../../configureMockStore';
 
-import PageDetails from '../PageDetails';
+import PageView from '../index';
 
 describe('Page screen', () => {
-  it('should render correctly an existing page', () => {
+  it('renders a message if the page does not exist', () => {
+    const store = mockStore({
+      pages: {
+        items: [],
+        isFetching: 1,
+      },
+    });
+    const match = {
+      params: {
+        pageId: '1',
+      },
+    };
+    expect(renderer.create(<PageView store={store} fetchPages={jest.fn()} match={match} />)).toMatchSnapshot();
+  });
+
+  it('renders existing pages', () => {
     const store = mockStore({
       pages: {
         items: [
@@ -17,6 +32,22 @@ describe('Page screen', () => {
               rendered: 'Some page tile',
             },
             slug: 'some-page-slug',
+            content: {
+              rendered: 'Some page content ot be rendered',
+            },
+            link: 'some-page-link',
+            author: 54,
+            comment_status: 'dunno',
+            featured_media: 43,
+            status: 'some-status',
+          },
+          {
+            id: 3,
+            parent: 34,
+            title: {
+              rendered: 'Some other page tile',
+            },
+            slug: 'some-other-page-slug',
             content: {
               rendered: 'Some page content ot be rendered',
             },
@@ -46,12 +77,12 @@ describe('Page screen', () => {
         featured_media: 43,
         status: 'some-status',
       },
-      match: {
-        params: {
-          postId: 'some-post-id',
-        },
-      },
     });
-    expect(renderer.create(<PageDetails store={store} fetchPosts={jest.fn()} />)).toMatchSnapshot();
+    const match = {
+      params: {
+        pageId: '1',
+      },
+    };
+    expect(renderer.create(<PageView store={store} fetchPages={jest.fn()} match={match} />)).toMatchSnapshot();
   });
 });
