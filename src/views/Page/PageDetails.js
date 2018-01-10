8e44fec375825978aca010ab92e2d7913b2fe3b3
FlorinaIe <florina.ieremciuc@gmail.com>
2018-01-10 11:58:09 +0200

    refactor page details to functional component

--- src/views/Page/PageDetails.js
+++ src/views/Page/PageDetails.js
@@ -1,16 +1,15 @@
 import 'react-router-modal/css/react-router-modal.css';
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { Container, Image, Header, Button } from 'semantic-ui-react';
 import Moment from 'react-moment';
 
 import { pagePropType } from '../SideMenu/reducer';
 import './style.css';
 
-class PageDetails extends Component {
-  getImage(sourceImg) {
+const PageDetails = (props) => {
+  function getImage(sourceImg) {
     let imageSource;
     if (sourceImg) {
       imageSource = sourceImg[0].source_url;
@@ -19,37 +18,31 @@ class PageDetails extends Component {
     return imageSource;
   }
 
-  render() {
-    const { page } = this.props;
-    const { author } = page._embedded;
-    const featuredMedia = page._embedded['wp:featuredmedia'];
+  const { page } = props;
+  const { author } = page._embedded;
+  const featuredMedia = page._embedded['wp:featuredmedia'];
 
-    return (
-      <Container className="page">
-        <Header dividing>
-          <Link to="/">
-            <Button size="huge" icon="close" />
-          </Link>
-          <div dangerouslySetInnerHTML={{ __html: page.title.rendered }} />
-        </Header>
-        <Header.Subheader>
-          &nbsp;by&nbsp;<b>{author[0].name}</b>,&nbsp;<Moment format="MMMM DD, YYYY">{page.date}</Moment>
-        </Header.Subheader>
-        {featuredMedia ? <Image src={this.getImage(featuredMedia)} /> : ''}
-        <Container textAlign="justified">
-          <div dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
-        </Container>
+  return (
+    <Container className="page">
+      <Header dividing>
+        <Link to="/">
+          <Button size="huge" icon="close" />
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: page.title.rendered }} />
+      </Header>
+      <Header.Subheader>
+        &nbsp;by&nbsp;<b>{author[0].name}</b>,&nbsp;<Moment format="MMMM DD, YYYY">{page.date}</Moment>
+      </Header.Subheader>
+      {featuredMedia ? <Image src={getImage(featuredMedia)} /> : ''}
+      <Container textAlign="justified">
+        <div dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
       </Container>
-    );
-  }
-}
+    </Container>
+  );
+};
 
 PageDetails.propTypes = {
   page: pagePropType.isRequired,
 };
 
-const mapStateToProps = state => ({
-  sideMenuVisible: state.sideMenuVisible,
-});
-
-export default connect(mapStateToProps, null)(PageDetails);
+export default PageDetails;
